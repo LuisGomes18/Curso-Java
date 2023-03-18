99f313a239e6378f741d601d173dd3ae9852d31f
LuisGomes18 <luiscogomes@edu.madeira.gov.pt>
2023-03-18 20:32:13 +0000

    Add files via upload

--- Curso de Java/src/PrimeiroPrograma.java
+++ Curso de Java/src/PrimeiroPrograma.java
@@ -1,5 +1,8 @@
 public class PrimeiroPrograma {
+    // psvm + tab
     public static void main(String[] args) {
         System.out.println("Olá, mundo!");
+        // sout + tab
+        System.out.println("Oi");
     }
 }
